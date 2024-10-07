e44e5f931a22ab0a678ff3b2124ac44841997b03
Ikechi Emmanuel <ikechiemmanuelice.gmail.com>
2024-10-07 23:55:52 +0100

    Added Basic Functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,7 @@
 let contactMessage = document.getElementsByClassName("contact-message-details");
 let chatSection = document.querySelector(".chat-section");
 let contactChat = document.querySelector(".contact-chat");
+let exitChat = document.querySelector(".return");
 for (let i = 0; i < contactMessage.length; i++) {
     contactMessage[i].addEventListener ("click", () => {
         console.log(contactMessage[i]);
@@ -12,4 +13,11 @@ for (let i = 0; i < contactMessage.length; i++) {
     });
 }
 
-document.querySelector("body").style.overflow = "hidden";
+exitChat.addEventListener("click", ()=> {
+        chatSection.style.left = "0";
+        contactChat.style.display = "block";
+        // contactChat.style.width = "100vw";
+        contactChat.style.left = "100%";
+        contactChat.style.transition = "all linear .2s";
+        chatSection.style.transition = "all linear .3s .2s";
+})
